f18f8c546846aadeed1e6110b7280fc161e91eac
JosiahDub <josiahwhite@gmail.com>
2018-04-01 13:18:31 -0400

    Adding RegEx files. Created framework for representative intent. It currently prints their names.

--- __init__.py
+++ __init__.py
@@ -1,14 +1,40 @@
+# built-in
+import json
+# 3rd party
+import requests
+# Mycroft stuff
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
 
 
 class StateRepresentativeSkill(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super(StateRepresentativeSkill, self).__init__(name="StateRepresentativeSkill")
+        self.url = 'https://whoismyrepresentative.com/getall_mems.php'
 
-    @intent_handler(IntentBuilder().require('StateRepresentative'))
+    def initialize(self):
+        pass
+
+    @intent_handler(IntentBuilder("")
+                    .require('StateRepresentativeKeyword')
+                    .require('zip')
+                    )
     def handle_state_representative(self, message):
+        print("in state rep")
+        zip_code = message.data.get("zip")
+        result = requests.get(self.url, params={"zip": zip_code,
+                                                "output": 'json'})
+        if result.status_code == 200:
+            content = json.loads(result.content)["results"]
+        else:
+            # handle it
+            content = {}
+        for critter in content:
+            print(critter["name"])
         self.speak_dialog('state.representative')
+
+    def stop(self):
+        pass
 
 
 def create_skill():
