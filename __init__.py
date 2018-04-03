beaffb9eff6f0695592752fdb53473eaf14011c5
josiahdub <josiahwhite@gmail.com>
2018-04-02 22:47:17 -0400

    Improving the dialog.

--- __init__.py
+++ __init__.py
@@ -29,9 +29,31 @@ class StateRepresentativeSkill(MycroftSkill):
         else:
             # handle it
             content = {}
+        sens = []
+        reps = []
         for critter in content:
-            print(critter["name"])
-        self.speak_dialog('state.representative')
+            if "senate" in critter["link"]:
+                sens.append(critter["name"])
+            else:
+                reps.append(critter["name"])
+        dialog = ''
+        if len(reps) == 1:
+            dialog += "Your representative is " + reps[0] + "."
+        elif len(reps) > 1:
+            dialog += "Your representatives are " + self.oxford_comma(reps) + "."
+        if len(sens) == 1:
+            dialog += " Your senator is " + sens[0] + "."
+        elif len(sens) == 2:
+            dialog += " Your senators are " + self.oxford_comma(sens) + "."
+        self.speak(dialog)
+
+    @staticmethod
+    def oxford_comma(name_list):
+        if name_list == 2:
+            string = name_list[0] + " and " + name_list[1]
+        elif name_list > 2:
+            string = ", ".join(name_list[:-1]) + ", and " + name_list[-1]
+        return string
 
     def stop(self):
         pass
